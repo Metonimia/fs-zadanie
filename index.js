8ba9f4c5f14dbe84b67253d286cf587fcef25ca2
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-01-06 22:37:07 +0100

    Added project

---
+++ index.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readdir('../fs-zadanie', function(err, files) {
+	console.log(files);
+	fs.writeFile('text.txt', files, (err) => {
+		if (err) throw err;
+		console.log('The file has been saved!');
+	});
+});
